b82b40449721d89e6d71469feaa8710f7155940f
pie-jhs6 <jhs6@torder.com>
2024-03-18 23:58:24 +0900

    feat: spring application context bean call method 1. name 2. beanType

---
+++ src/test/java/basic/core/beanfind/ApplicationContextBasicFindTest.java
@@ -0,0 +1,56 @@
+package basic.core.beanfind;
+
+import basic.core.AppConfig;
+import basic.core.member.MemberService;
+import basic.core.member.MemberServiceImpl;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.NoSuchBeanDefinitionException;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+/**
+ * packageName    : basic.core.beanfind
+ * fileName       : ApplicationContextBasicFindTest
+ * author         : janghyoseong
+ * date           : 3/18/24
+ * description    :
+ * ===========================================================
+ * DATE              AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * 3/18/24        janghyoseong       최초 생성
+ */
+public class ApplicationContextBasicFindTest {
+
+    AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+    @Test
+    @DisplayName("빈 이름으로 조회")
+    void findBeanByName() {
+        MemberService memberService = ac.getBean("memberService", MemberService.class);
+        Assertions.assertThat(memberService).isInstanceOf(MemberServiceImpl.class);
+    }
+
+    @Test
+    @DisplayName("이름 없이 타입으로만 조회")
+    void findBeanByType() {
+        MemberService memberService = ac.getBean(MemberService.class);
+        Assertions.assertThat(memberService).isInstanceOf(MemberServiceImpl.class);
+    }
+
+    @Test
+    @DisplayName("구체 타입으로 조회") // 구현체로 구현하면 유연성이 부족
+    void findBeanByName2() {
+        MemberService memberService = ac.getBean("memberService", MemberServiceImpl.class);
+        Assertions.assertThat(memberService).isInstanceOf(MemberServiceImpl.class);
+    }
+
+    @Test
+    @DisplayName("빈 이름으로 조회 X")
+    void findBeanByNameX() {
+        assertThrows(NoSuchBeanDefinitionException.class, () ->
+                ac.getBean("xxxx", MemberService.class));
+    }
+}
